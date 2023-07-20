2eddbec4cfcdc47f91fe65ecdb77557a9c6a360f
Alexander Shkurenkov <a.shkurenkov@gmail.com>
2023-07-20 16:27:32 +0300

    Stack deploy has been added.

--- up.sh
+++ up.sh
@@ -6,4 +6,11 @@ tend=`date +%s.%N`
 truntime=$( echo "$tend - $tstart" | bc -l )
 echo -e "\nTerraform execution time: $(date -d @$truntime -u +%H:%M:%S.%N)"
 
+echo -e "\nImport kubectl config."
 yc container cluster get-credentials kub-test --external
+
+astart=`date +%s.%N`
+cd ansible/; ansible-playbook base.yml
+aend=`date +%s.%N`
+aruntime=$( echo "$aend - $astart" | bc -l )
+echo -e "\nAnsible execution time: $(date -d @$aruntime -u +%H:%M:%S.%N)"
